b1e8fbede24af90ed6e5340235f0e2d7805343e6
Pedro Alburqueque <prolsu88@gmail.com>
2020-10-26 20:16:02 -0400

    scores are locally stored and shown at main page (if any)

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-document.getElementById("readyBtn").addEventListener("click", startQuiz);
+document.querySelector("#readyBtn").addEventListener("click",askQuestions);
+        
+var userInitials = localStorage.getItem("initials");
+var userScore = localStorage.getItem("score");
+    
+if (userInitials !== null && userScore !== null){
+    document.querySelector("#highScores").setAttribute("style", "display:;");
+    var newH3Tag = document.createElement("H3");
+    newH3Tag.textContent = "User: "+ userInitials + " Score: " + userScore;
+    document.querySelector("#newScore").appendChild(newH3Tag);
+}; 
 
-function startQuiz() {
+
+function askQuestions() {
     var timeEl = document.querySelector("#clock");
     var timerM = "10";
     var timerS = "00";
     timeEl.textContent = "Time left: " + timerM + ":" + timerS;
     
-    //---TIMER GOES HERE 
     var clockF = setInterval(function() {
         timerS--;
         
@@ -32,51 +42,57 @@ function startQuiz() {
         timeEl.textContent = "Time left: "+ timerM + ":" + timerS;
         
     },1000);
+
+    document.querySelector("#welcomePage").setAttribute("style", "display: none;");
+    document.getElementById("myQ1").setAttribute("style", "display:;");
     
     var myScoreIs = document.querySelector("#myScoreIs");
     var theRightAnswers = document.querySelectorAll(".correct"); 
     var theWrongAnswers = document.querySelectorAll(".choice");
     var questionHolders = document.querySelectorAll(".outsideWrapper");
     var currentQuestion = document.querySelectorAll(".outsideWrapper");
+    
     var q = 0;
     var a = 0;
     var myScore = 0;
 
-    document.getElementById("welcomePage").setAttribute("style", "display: none;");
-    document.getElementById("myQ1").setAttribute("style", "");
-
     for (var i = 0; i < theRightAnswers.length; i++){
        theRightAnswers[i].addEventListener("click", rightAnswer);
-       
     };
+
     for (var i = 0; i < theWrongAnswers.length; i++){
        theWrongAnswers[i].addEventListener("click", wrongAnswer);
-       
     };
+
     function showMyResuts(){
         document.querySelector("#results-here").setAttribute("style", "display: ;");
-        document.querySelector("#questionsAndChoices").setAttribute("style", "display: none;");
+        //document.querySelector("#questionsAndChoices").setAttribute("style", "display: none;");
+        for(var i=0;i<questionHolders.length;i++){
+            questionHolders[i].setAttribute("style", "display: none;");
+        }
         myScoreIs.textContent = myScore
-    }
-
+        q = 0;
+        a = 0;
+    };
+    
     function rightAnswer(){
         currentQuestion[a].setAttribute("style", "display: none;")
         a++
         if(q < 24){
             questionHolders[q].nextElementSibling.setAttribute("style", "display: ;");
             q++   
+            myScore = myScore + 4;    
+            console.log(myScore);
         }
         if(q == 24){
             clearInterval(clockF)
             showMyResuts();
+            questionHolders[24].setAttribute("style","display: none;");
         }
         if (timerM == 0 && timerS == 0){
             showMyResuts();
         }
-            
-        console.log("right!");
-        myScore = myScore + 4;
-        console.log(myScore);
+        
     };
 
     function wrongAnswer(){
@@ -89,6 +105,7 @@ function startQuiz() {
         if(q == 24){
             clearInterval(clockF)
             showMyResuts();
+            questionHolders[24].setAttribute("style","display: none;");
         }
         timerS = timerS - 30
         if(timerS < 0 ){
@@ -102,4 +119,13 @@ function startQuiz() {
         console.log("wrong!");
     };
     
+    document.querySelector("#submitButton").addEventListener("click", function(){
+        var myInitials = document.querySelector("#myInitials").value;
+        
+        localStorage.initials = myInitials;
+        localStorage.score = myScore;
+        myScore = 0;  
+        location.reload();     
+        
+        });
 };
